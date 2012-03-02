2ec487e10d60e4c25434d3180673a41a89e467d0
spanner3003 <spanner3003@gmail.com>
2012-03-02 21:27:42 +0000

    changed to hopefully work better with jenkins

--- build.sh
+++ build.sh
@@ -96,9 +96,7 @@ START=$(date +%s)
 lunch $LUNCH
 
 # Kernel build
-cd kernel/samsung/p1
-./build.sh $P1_TARGET
-cd ../../..
+$ANDROID_BUILD_TOP/kernel/samsung/p1/build.sh $P1_TARGET
 cp $ANDROID_BUILD_TOP/device/samsung/galaxytab/kernel-$P1_TARGET $ANDROID_BUILD_TOP/device/samsung/galaxytab/kernel
 
 # Android build
